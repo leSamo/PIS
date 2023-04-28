feced583e326350cec5217016cef5b8d61781c87
student <xfindr00@vutbr.cz>
2023-04-28 14:45:19 +0200

    base for jwt token auth

---
+++ BE/rest-api/src/main/java/cz/xoleks00/pis/jwt/JAXRSConfiguration.java
@@ -0,0 +1,19 @@
+package cz.xoleks00.pis.jwt;
+
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint with authorization.
+ *
+ * @author burgetr
+ */
+@ApplicationPath("resources")
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({ "admin", "staff", "customer" })
+public class JAXRSConfiguration extends Application {
+
+}
---
+++ BE/rest-api/src/main/java/cz/xoleks00/pis/jwtlogin/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package cz.xoleks00.pis.jwtlogin;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint with authorization.
+ *
+ * @author burgetr
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
